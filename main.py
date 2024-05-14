d2f5bc36cccb528816a93e6ac355a69662530655
Vadim <vadimpapusha2310@gmail.com>
2024-05-14 21:36:04 +0300

    виконав дз

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+import uvicorn
 import sqlite3
 from databases import Database
 from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
@@ -11,18 +12,24 @@ database = Database(DATABASE_URL)
 
 app = FastAPI()
 
+
 users = Table('users',
-              metadata,
-              Column('id', Integer, primary_key=True),
-              Column('name', String),
+             metadata,
+             Column('id', Integer, primary_key=True),
+             Column('name', String),
               Column('surname', String),
               Column('job', String(10))
-              )
+ )
+
+metadata.create_all(DATABASE)
+
+DATABASE.connect()
 
 
 @app.on_event('startup')
 async def startup():
     await database.connect()
+
 
 
 @app.on_event('shutdown')
@@ -31,8 +38,9 @@ async def shutdown():
 
 
 @app.get('/users/{user_id}')
-async def read_user(user_id: int):
+async def read_user(user_id: int = Path(..., title="user's id", description='This is the id of current user'),
+                    text: str = Query('', title = '',text = '')):
     query = users.select().where(users.c.id == user_id)
     user = await database.fetch_one(query)
-    return {'user': user}
-
+    return {'user': user,
+            'text': text}
