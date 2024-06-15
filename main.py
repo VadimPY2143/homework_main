512e2d0983e1b56c30ec7e46bab1bef3b7829914
Vadim <vadimpapusha2310@gmail.com>
2024-06-15 14:45:57 +0300

    дз за попередній урок

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, HTTPException
 import uvicorn
-import sqlite3
-from databases import Database
-from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
-import asyncio
-
-DATABASE_URL = 'sqlite:///test.db'
-DATABASE = create_engine(DATABASE_URL)
-metadata = MetaData()
-database = Database(DATABASE_URL)
+from pydantic import BaseModel, ValidationError, field_validator
+from typing import Any
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-users = Table('users',
-             metadata,
-             Column('id', Integer, primary_key=True),
-             Column('name', String),
-              Column('surname', String),
-              Column('job', String(10))
- )
+class Item(BaseModel):
+    title: str
+    author: str
+    year: int
+    isbn: str
 
-metadata.create_all(DATABASE)
+    @field_validator('title')
+    @classmethod
+    def title_check(cls, value: Any):
+        if len(value) == 0:
+            raise ValueError("Це поле є обов'язкове")
+        return value
 
-DATABASE.connect()
+    @field_validator('author')
+    @classmethod
+    def author_check(cls, value: Any):
+        if len(value) == 0:
+            raise ValueError("Це поле є обов'язкове")
+        return value
+
+    @field_validator('year')
+    @classmethod
+    def year_check(cls, value: Any):
+        if value >= 1500 and value <= 2024:
+            return value
+        raise ValueError("Неправильний рік")
 
 
-@app.on_event('startup')
-async def startup():
-    await database.connect()
+    @field_validator('isbn')
+    @classmethod
+    def isbn_check(cls, value: Any):
+        if len(value) == 13 or len(value) == 0:
+            return value
+        else:
+            raise ValueError("Неправильна інформація")
 
 
+books_base = []
 
-@app.on_event('shutdown')
-async def shutdown():
-    await database.disconnect()
+@app.post("/items/")
+def create_item(item: Item):
+    books_base.append(item)
+    return item
+
+@app.get('/books')
+def get_books():
+    return books_base
 
 
-@app.get('/users/{user_id}')
-async def read_user(user_id: int = Path(..., title="user's id", description='This is the id of current user'),
-                    text: str = Query('', title = "user's description",description = "This is the user's description")):
-    query = users.select().where(users.c.id == user_id)
-    user = await database.fetch_one(query)
-    return {'user': user_id,
-            'text': text}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
